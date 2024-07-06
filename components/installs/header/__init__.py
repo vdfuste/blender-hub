b15700d46d8a95e8c749c256e2f2349e57b9c6ec
Víctor Dual <vdfuste@gmail.com>
2024-07-06 18:44:11 -0400

    sjkfdvnb

--- components/installs/header/__init__.py
+++ components/installs/header/__init__.py
@@ -15,11 +15,29 @@ class VersionButtonsHeader(QWidget):
 		layout.setSpacing(0)
 		layout.setAlignment(Qt.AlignBottom)
 		
-		for index, version in enumerate(series):
-			button = QPushButton(f"Version {version}")
-			button.setObjectName("version-btn")
-			button.clicked.connect(lambda: selectVersion(index))
+		# TO-DO: Fix this!
+		#for index, version in enumerate(series):
+		#	button = QPushButton(f"Version {version}")
+		#	button.setObjectName("version-btn")
+		#	button.clicked.connect(lambda: selectVersion(index))
 
-			layout.addWidget(button)
+		#	layout.addWidget(button)
+
+		# DELETE THIS LATER
+		button4X = QPushButton(f"Serie 4.X")
+		button4X.setObjectName("version-btn")
+		button4X.clicked.connect(lambda: selectVersion(0))
+		layout.addWidget(button4X)
+
+		button3X = QPushButton(f"Serie 3.X")
+		button3X.setObjectName("version-btn")
+		button3X.clicked.connect(lambda: selectVersion(1))
+		layout.addWidget(button3X)
+
+		#button2X = QPushButton(f"Serie 4.X")
+		#button2X.setObjectName("version-btn")
+		#button2X.clicked.connect(lambda: selectVersion(0))
+		#layout.addWidget(button2X)
+		# DELETE THIS LATER
 
 		self.setLayout(layout)
